f60afa740da6af893dba94f03faf22fcfaaccf84
Nicholas Johnson <nejohnson2@gmail.com>
2018-12-20 10:12:45 -0500

    changed download file

--- satfills/ls_download.py
+++ satfills/ls_download.py
@@ -9,9 +9,12 @@ by Nicholas E. Johnson
 '''
 import json, os
 import subprocess
+import argparse
 import pandas as pd 
 
-def download_data(sceneID, clip=None, dpath='./download/'):
+parser = argparse.ArgumentParser()
+
+def download_data(sceneID, dpath, clip=None):
 	'''Run landsat download utility
 
 	Parameters
@@ -29,61 +32,49 @@ def download_data(sceneID, clip=None, dpath='./download/'):
 	'''
 	
 	if not os.path.exists(dpath):
-    	os.makedirs(dpath)
+		os.makedirs(dpath)
 
 	command = ['landsat', 'download', '-d', dpath]
-
+	
 	if type(sceneID) == str:
 		command.append(sceneID)
 	else:
 		command.extend(sceneID)	
 
-	subprocess.call(command)
-	return
+	return subprocess.call(command)
 
-def get_sceneID(pathrow=None, latlng=None, clouds=10):
-	'''Run landsat search utility to extract sceneID
+def get_sceneID(latlng, clouds):
+	'''Run landsat search utility to extract sceneID from lat/lng
 
 	Parameters
 	----------
-	pathrow : str,
-		Paths and Rows in order separated by comma. Use quotes "001,003".
-        Example: path,row,path,row 001,001,190,204
+	latlng : list,
+		Latitude and longitude coordinates
 
-    latlng : list,
-    	Latitude and longitude coordinates
+	clouds : int,
+		Maximum percentage of cloud cover allowed.  Default is 10.
 
-    clouds : int,
-    	Maximum percentage of cloud cover allowed.  Default is 10.
-
-    Returns
-    -------
-    sceneID : string,
-    	Example: LC81660392014196LGN00
+	Returns
+	-------
+	sceneID : string,
+		Example: LC81660392014196LGN00
 
 	'''
-	try:
-		if pathrow != None:
-			search = ['-p ']
-			search.insert(1, pathrow)
-		elif latlng != None:
-			search = ['--lat', '--lon']
-			search.insert(1, str(latlng[0]))
-			search.insert(3, str(latlng[1]))
-		else:
-			raise Exception('No input')
-	except Exception as e:
-		print e
-		return
 
-	command = ['landsat', 'search', '--geojson', '--cloud', str(clouds), '--latest', str(10)]
+	search = ['--lat', '--lon']
+	search.insert(1, str(latlng[0]))
+	search.insert(3, str(latlng[1]))
+
+	command = ['landsat', 'search', '--json', '--cloud', str(clouds), "--end", "january 01 2015"]
 	command.extend(search)
 	
 	output = subprocess.check_output(command)
-	results = json.loads(output)
-	print results['results'][0]
 	
-	return 
+	results = json.loads(output)
+	print "Found {} images".format(results['total_returned'])
+
+	return results['results'][0]['sceneID']
+
 
 def unzip():
 	'''Unzip any .tar.gz files in a directory'''
@@ -93,17 +84,24 @@ def create_mask():
 	'''Clip Landsat image with landfill bounds'''
 	with rasterio.open("tests/data/RGB.byte.tif") as src:
 		out_image, out_transform = rasterio.tools.mask.mask(src, features,
-	                                                    crop=True)
+														crop=True)
 		out_meta = src.meta.copy()
 
-def main():
+if __name__ == '__main__':
+	parser.add_argument('--outPath', action='store', dest='outPath', required=True, 
+		help='Output file path')    
+	parser.add_argument('--clouds', action='store', dest='clouds', default=10, 
+		help='Amount of acceptable cloud cover')    
+
+	args = parser.parse_args()
+
 	fname = '../data/output/landfills_master.csv'
 	data = pd.read_csv(fname)
 
 	for coords in data[['lat','lng']].values:
-		sceneID = get_sceneID(latlng=coords)
+		
+		sceneID = get_sceneID(latlng=coords, clouds=args.clouds)
 
-	#clip = data['clip']
-	
-		download_data(sceneID=scene, clip=clip)
+		download_data(sceneID=str(sceneID), dpath=args.outPath)
+		break
 	
